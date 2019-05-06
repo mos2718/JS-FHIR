d06c6bf76c79ad31c732f6f23f82c39e70b6f18e
chhsiao <chhsiao@gms.tcu.edu.tw>
2019-05-06 10:46:41 +0800

    Q

--- JSON/setting.js
+++ JSON/setting.js
@@ -2,4 +2,10 @@
 //5. FHIR TW with HTTPS  https://hapi.fhir.tw/
 // H707 實驗室  http://203.64.84.182:8036/
 var rootOrgID = "1856069";
+
+
+//org patient
 //http://hapi.fhir.org/baseDstu3/Patient?organization=1856069
+
+//sub org 1902264
+http://hapi.fhir.org/baseDstu3/Organization?partof=1856069
