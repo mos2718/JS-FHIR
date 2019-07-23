a8ad0e085b8f4c6fa51a51884c4ad46ad30ad14e
chhsiao <chhsiao@gms.tcu.edu.tw>
2019-07-23 16:37:36 +0800

    C

--- JSON/setting.js
+++ JSON/setting.js
@@ -1,16 +1,20 @@
-﻿var FHIRrootURL = "http://hapi.fhir.org/baseDstu3";
+﻿var FHIRrootURL = "http://ymu041-190.ym.edu.tw:58080/baseDstu3";
 
-    // 成大 "http://140.116.177.26:8080/";
+// "http://hapi.fhir.org/baseDstu3";
 
+/*
+ 系塔:  http://hapi.fhir.tw/baseDstu3
+ 陽明:  http://ymu041-190.ym.edu.tw:58080/baseDstu3/
+ 成大:  http://140.116.177.26:8080/baseDstu3 
+*/
+
+/*	
+Docker版本使用: https://hub.docker.com/r/jtmille3/hapi-fhir-dstu3-v2.5-postgres  */
 //
 
 //5. FHIR TW with HTTPS  https://hapi.fhir.tw/
 // H707 實驗室  http://203.64.84.182:8036/
 //http://hapi.fhir.org/baseDstu3/Observation
-/*
-成大 FHIR server 已裝好，網址 :
-http://140.116.177.26:8080/  
-*/
 
 /* Happy FHIR solution
 //http://hapi.fhir.org/baseDstu3
@@ -19,7 +23,8 @@ var rootOrgID = "1856069";
 //http://hapi.fhir.org/baseDstu3/Patient?organization=1856069
 
 // post observation
-//http://hapi.fhir.org/baseDstu3/Observation
+//http://hapi.fhir.org/baseDstu3/Observation/1979373
+
 
 //http://hapi.fhir.org/baseDstu3/Organization?partof=1856069
 // error http://hapi.fhir.org/baseDstu3/Organization?partOf=1856069
@@ -27,14 +32,17 @@ var rootOrgID = "1856069";
 /* get patient 1856173 observations
    http://hapi.fhir.org/baseDstu3/Observation?subject=1856173
    get observation 1856172
-   http://hapi.fhir.org/baseDstu3/Observation?subject=1856172
+   http://hapi.fhir.org/baseDstu3/Observation?subject=
+   
 
 */
 
 /*
  http://hapi.fhir.org/baseDstu3/Organization/1923622/_history/1
  
- http://hapi.fhir.org/baseDstu3/Patient/1923623/_history/1
+ http://hapi.fhir.org/baseDstu3/Patient/1979380
+ 
+ /_history/1
 
 */
 /*
