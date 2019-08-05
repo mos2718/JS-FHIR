c1431228988e15da8587a66dc7a6a9fc993aa6bc
JackNB2 <mos2718@gms.com>
2019-08-05 09:44:36 +0800

    C

--- JSON/setting.js
+++ JSON/setting.js
@@ -1,65 +1,23 @@
 ﻿var FHIRrootURL = "http://hapi.fhir.org/baseDstu3";
 
-// "http://hapi.fhir.org/baseDstu3";
+// "http://hapi.fhir.org";
 
-/*
- 系塔:  http://hapi.fhir.tw/baseDstu3
- 陽明:  http://fhir.dicom.org.tw/baseDstu3
- 成大:  http://140.116.177.26:8080/baseDstu3 
-*/
+/* 
+post Patioent, Observation....
+ http://hapi.fhir.org/baseDstu3/Patient
+ http://hapi.fhir.org/baseDstu3/Observation
 
-/*	
-Docker版本使用: https://hub.docker.com/r/jtmille3/hapi-fhir-dstu3-v2.5-postgres  */
-//
+get org 1856069  patient
+http://hapi.fhir.org/baseDstu3/Patient?organization=1856069
 
-//5. FHIR TW with HTTPS  https://hapi.fhir.tw/
-// H707 實驗室  http://203.64.84.182:8036/
-//http://hapi.fhir.org/baseDstu3/Observation
-
-/* Happy FHIR solution
-//http://hapi.fhir.org/baseDstu3
-var rootOrgID = "1856069";
-//get org patient
-//http://hapi.fhir.org/baseDstu3/Patient?organization=1856069
-
-// post observation
-//http://hapi.fhir.org/baseDstu3/Observation/1979373
-
-
-//http://hapi.fhir.org/baseDstu3/Organization?partof=1856069
-// error http://hapi.fhir.org/baseDstu3/Organization?partOf=1856069
-
-/* get patient 1856173 observations
+get patient 1856173 observations
    http://hapi.fhir.org/baseDstu3/Observation?subject=1856173
-   get observation 1856172
-   http://hapi.fhir.org/baseDstu3/Observation?subject=
-   
-
-*/
-
-/*
- http://hapi.fhir.org/baseDstu3/Organization/1923622/_history/1
- 
- http://hapi.fhir.org/baseDstu3/Patient/1979380
- 
- /_history/1
-
-*/
-/*
-花蓮饕客
-http://hapi.fhir.org/baseDstu3/Organization/1905554/_history/1
-
-http://hapi.fhir.org/baseDstu3/Organization?partof=1905554
-
-花蓮吃 吃 吃
-http://hapi.fhir.org/baseDstu3/Organization/1905569/_history/1
-
-慈大一起吃 org ID
-1905582
-所屬病人(客戶)
+  
+取得組織所屬病人
 http://hapi.fhir.org/baseDstu3/Patient?organization=1905582
 
-patient 1
-http://hapi.fhir.org/baseDstu3/Patient/1905593/_history/1
+取得附屬組織
+http://hapi.fhir.org/baseDstu3/Organization?partof=1905554
+
 
 */
